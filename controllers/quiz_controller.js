87ece33f6d0d46b59551babc3f3377ed7ba5b6bc
benitojcv <benitojcv@gmail.com>
2015-06-24 13:31:32 +0100

    Ejercicio obligatorio Tema 6. Inclusion de pagina de creditos.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,22 @@
 
-
-exports.question = function(req,res) {
-	res.render('quizes/question', { pregunta: 'Capital de Italia'});
+exports.question = function(req, res) {
+	res.render('quizes/question', {
+		pregunta : 'Capital de Italia'
+	});
 };
 
 exports.answer = function(req, res) {
 	if (req.query.respuesta === 'Roma') {
-		res.render('quizes/answer', { respuesta : 'Correcto'});
+		res.render('quizes/answer', {
+			respuesta : 'Correcto'
+		});
 	} else {
-		res.render('quizes/answer', { respuesta : 'Incorrecto'});
+		res.render('quizes/answer', {
+			respuesta : 'Incorrecto'
+		});
 	}
+};
+
+exports.author = function(req, res) {
+	res.render('author');
 };
