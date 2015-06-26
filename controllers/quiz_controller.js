37e0c9000da7f6255ab07b7db6a08b93795ae57a
benitojcv <benitojcv@gmail.com>
2015-06-26 10:05:31 +0200

    Lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,22 +1,27 @@
 var models = require('../models/models.js');
 
-
-exports.question = function(req, res) {
-	models.Quiz.findAll().then(function(quiz) {
-		res.render('quizes/question', { 
-			pregunta: quiz[0].pregunta
-		});
+exports.index = function(req,res) {
+	models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/index', { quizes : quizes });
 	});
 };
 
+exports.show = function(req, res) {
+	models.Quiz.findById(req.params.quizId).then(function(quiz) {
+		res.render('quizes/show', { quiz : quiz });
+	});	
+};
+
 exports.answer = function(req, res) {
-	models.Quiz.findAll().then(function(quiz) {
-		if (req.query.respuesta === quiz[0].respuesta) {
+	models.Quiz.findById(req.params.quizId).then(function(quiz) {
+		if (req.query.respuesta === quiz.respuesta) {
 			res.render('quizes/answer', {
+				quiz : quiz,
 				respuesta : 'Correcto'
 			});
 		} else {
 			res.render('quizes/answer', {
+				quiz : quiz, 
 				respuesta : 'Incorrecto'
 			});
 		}
