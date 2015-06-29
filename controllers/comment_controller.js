25950af5f3cd98f9999597e96a2463618af6c794
benitojcv <benitojcv@gmail.com>
2015-06-29 15:26:58 +0100

    Terminada funcionalidad de comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,7 +1,6 @@
 var models = require('../models/models.js');
 
 exports.new = function(req, res) {
-	console.log("***Nuevo comentario");
 	res.render('comments/new', {
 		quizid: req.params.quizId, 
 		errors: []
@@ -10,6 +9,31 @@ exports.new = function(req, res) {
 
 exports.create = function(req, res) {
 	var comment = models.Comment.build({
-		texto: "TODO"
+		texto: req.body.comment.texto,
+		QuizId: req.params.quizId
+	});
+	
+	console.log(comment);
+	
+	comment
+	.validate().then(function(err) {
+		if (err) {
+			console.error("ERROR:" + err.errors);
+			
+			res.render('comments/new', { 
+				comment: comment, 
+				errors: err.errors,
+				quizid: req.params.quizId
+			});
+		} else {
+			
+			comment
+			.save().then(function() {
+				console.log("Insertado!!!");
+				res.redirect('/quizes/' + req.params.quizId);
+			});			
+		}
+	}).catch(function(error) {
+		next(error);
 	});
 };
