4ebbee4663f6d92c29d45b34216b0eb09f5f9add
benitojcv <benitojcv@gmail.com>
2015-07-01 16:01:52 +0100

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 // Autoload
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load);
 
 router.get('/login', sessionController.new);
 router.post('/login', sessionController.create);
@@ -31,5 +32,7 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+
+router.put('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
